24f6a898030508538b62aa97da9fe3d19ded2125
Zdravko Zdravkov <zdr0@yahoo.com>
2021-06-08 15:42:02 +0200

    registering blockchain service

--- Block Chain App/src/Web/PaymentSystem.BlockChain.Web/Startup.cs
+++ Block Chain App/src/Web/PaymentSystem.BlockChain.Web/Startup.cs
@@ -40,6 +40,7 @@
 
             // Domain Services
             services.AddSingleton<ITransactionPool, TransactionPool>();
+            services.AddTransient<IBlockChainService, BlockChainService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
